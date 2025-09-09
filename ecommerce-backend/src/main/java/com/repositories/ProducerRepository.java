eccbc1f0c0dc6cd3da367433561a73dc9ea9a04b
Jhonny <jhonatacastrozeboby@gmail.com>
2025-09-08 22:04:25 -0300

    feat: Adicionar método para encontrar produtor pelo ID do Supabase no ProducerRepository

--- ecommerce-backend/src/main/java/com/repositories/ProducerRepository.java
+++ ecommerce-backend/src/main/java/com/repositories/ProducerRepository.java
@@ -1,10 +1,16 @@
 package com.repositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.domain.Client;
 import com.domain.Producer;
 
 public interface ProducerRepository extends JpaRepository<Producer, Long>{
 
 	Producer findByEmail(String email);
+
+	Optional<Producer> findBySupabaseId(UUID supabaseId);
 }
