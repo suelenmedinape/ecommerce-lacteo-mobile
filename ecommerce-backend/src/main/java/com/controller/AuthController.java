d1156f04c2f9d4847674d1b364ba93692fc02583
Jhonny <jhonatacastrozeboby@gmail.com>
2025-09-08 22:08:32 -0300

    refactor: Remova importações não utilizadas e exclua o arquivo de importação SQL obsoleto

--- ecommerce-backend/src/main/java/com/controller/AuthController.java
+++ ecommerce-backend/src/main/java/com/controller/AuthController.java
@@ -5,8 +5,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
