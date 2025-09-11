f09c7ad5ea48cd0b4d2e124446ec2b2b513f8d69
Jhonny <jhonatacastrozeboby@gmail.com>
2025-09-10 21:19:02 -0300

    feat: Implement user role retrieval from JWT token

--- ecommerce-mobile/lib/all/service/user_logado.dart
+++ ecommerce-mobile/lib/all/service/user_logado.dart
@@ -1,4 +1,6 @@
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> salvarLogin(String token) async {
   final prefs = await SharedPreferences.getInstance();
@@ -23,4 +25,31 @@ Future<String?> getToken() async {
 Future<String?> getRole() async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getString('user_role');
+}
+
+Future<String?> getUserRoleFromToken() async {
+  final supabase = Supabase.instance.client;
+  
+  final Session? session = supabase.auth.currentSession;
+  
+  if (session == null) {
+    return null;
+  }
+  
+  final String accessToken = session.accessToken;
+  
+  try {
+    final Map<String, dynamic> decodedToken = JwtDecoder.decode(accessToken);
+    
+    final userMetadata = decodedToken['user_metadata'];
+    
+    if (userMetadata != null && userMetadata['role'] != null) {
+      return userMetadata['role'] as String;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    print('Erro ao decodificar o token: $e');
+    return null;
+  }
 }
